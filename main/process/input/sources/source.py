2cf8f49996ce431e6e2997c88f00f365d90cb151
Patrizio Guagliardo <patrizio.guagliardo@gmx.de>
2020-05-02 17:50:44 +0200

    error correction

--- main/process/input/sources/source.py
+++ main/process/input/sources/source.py
@@ -1,6 +1,4 @@
 from dataclasses import dataclass
-from main.process.configuration.config_reader import ConfigReader
-from main.process.configuration.transformation_config_reader import TransformationConfig
 from main.process.transformations.functions.transformations import Transformation_List
 from main.process.transformations.transformation import Transformations
 from main.YamlConfig.config import dataclass_json
@@ -15,17 +13,3 @@ class Source:
 
     def apply(self, spark_session):
         pass
-
-    def transformations(self, df):
-        if self.path:
-            config=ConfigReader(yaml_path=self.path)
-            return Transformations(
-                config=TransformationConfig.get_transformation_config(
-                    config=config.configuration
-                ),
-                transformation_cls=Transformation_List
-            ).apply(
-                df=df
-            )
-        else:
-            return df
